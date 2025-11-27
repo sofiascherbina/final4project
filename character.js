f664665f2c95b5fc851e2f66577d49d208d45a2f
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2025-11-27 21:50:04 +0200

    working

--- character.js
+++ character.js
@@ -5,7 +5,12 @@ selectorArr.forEach(el=>{
     let selectorText = el.querySelector('.selector');
     let listEl = el.querySelectorAll('.selector-list li');
     selectorText.addEventListener('click',()=>{
-        list.style.display = 'block';
+        if(list.style.display === 'block'){
+            list.style.display = 'none';
+        }
+        else{
+             list.style.display = 'block';
+        }
 });
     listEl.forEach(li =>{
             li.addEventListener('click',()=>{
@@ -14,3 +19,37 @@ selectorArr.forEach(el=>{
             });
         });
 });
+
+
+let chList = document.querySelector('.ch-container');
+let loadMore = document.querySelector('.load-more');
+let template = null;
+let page = 1;
+
+async function templateReady() {
+    let temp = await fetch('characters.hbs');
+    let res = await temp.text();
+    template = Handlebars.compile(res);
+}
+async function showCharacters(){
+    if (!template) await templateReady();
+    const characters = await fetchCharacter(page);
+    renderCharacter(characters.results);
+    page++;
+}
+loadMore.addEventListener('click', showCharacters);
+function fetchCharacter(page){
+    const params = new URLSearchParams({
+        page: page,
+    });
+    const url = new URL('https://rickandmortyapi.com/api/character')
+    url.search = params.toString();
+    return fetch(url)
+    .then(res=>res.json());
+}
+
+function renderCharacter(chArr){
+    const markup = template({characters: chArr});
+    chList.insertAdjacentHTML('beforeend', markup);
+}
+ showCharacters();
